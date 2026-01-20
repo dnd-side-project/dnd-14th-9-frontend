0ea3bea068988a56e4f604e66ccab146a7a7bfd1
tnemnorivnelee <tnemnorivnelee@gmail.com>
2026-01-21 00:16:43 +0900

    chore: 개발 환경 설정 (Prettier, ESLint 규칙 적용)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,27 @@
-import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import prettierConfig from "eslint-config-prettier";
+import prettierPlugin from "eslint-plugin-prettier";
+import { defineConfig } from "eslint/config";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+  {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      "prettier/prettier": "error",
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+      "no-unused-vars": "off", // TypeScript handles this
+      "@typescript-eslint/no-unused-vars": ["warn"],
+    },
+  },
+  prettierConfig,
+  {
+    ignores: [".next/*", "node_modules/*"],
+  },
 ]);
 
 export default eslintConfig;
