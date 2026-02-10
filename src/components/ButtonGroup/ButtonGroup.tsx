86e485afc156dd89cc7ae5b0cac0ee7b94bbba08
Rootjang92 <janggeunho7@gmail.com>
2026-02-10 14:57:08 +0900

    refactor: 불필요한 cva 제거

--- src/components/ButtonGroup/ButtonGroup.tsx
+++ src/components/ButtonGroup/ButtonGroup.tsx
@@ -1,23 +1,20 @@
-import { cva, type VariantProps } from "class-variance-authority";
 import { forwardRef, type HTMLAttributes } from "react";
 
 import { cn } from "@/lib/utils/utils";
 
-const BUTTONGROUP_VARIANTS = cva(["inline-flex", "flex-row", "flex-wrap", "items-center", "gap-2"]);
-
-export interface ButtonGroupProps
-  extends HTMLAttributes<HTMLDivElement>, VariantProps<typeof BUTTONGROUP_VARIANTS> {}
+export type ButtonGroupProps = HTMLAttributes<HTMLDivElement>;
 
 export const ButtonGroup = forwardRef<HTMLDivElement, ButtonGroupProps>(
-  ({ className, children, ...props }, ref) => {
-    return (
-      <div ref={ref} className={cn(BUTTONGROUP_VARIANTS({ className }))} role="group" {...props}>
-        {children}
-      </div>
-    );
-  }
+  ({ className, children, ...props }, ref) => (
+    <div
+      ref={ref}
+      className={cn("inline-flex flex-row flex-wrap items-center gap-2", className)}
+      role="group"
+      {...props}
+    >
+      {children}
+    </div>
+  )
 );
 
 ButtonGroup.displayName = "ButtonGroup";
-
-export { BUTTONGROUP_VARIANTS };
