2b851af1b0480948ac88c69e9e5c4646103c68f7
Rootjang92 <janggeunho7@gmail.com>
2026-02-17 09:22:33 +0900

    feat: 방 한줄소개 ui

--- src/features/session/components/SessionCreateForm.tsx
+++ src/features/session/components/SessionCreateForm.tsx
@@ -6,6 +6,7 @@ import { Input } from "@/components/Input/Input";
 
 export function SessionCreateForm() {
   const [roomName, setRoomName] = useState("");
+  const [roomDescription, setRoomDescription] = useState("");
 
   return (
     <form className="gap-xl flex w-full flex-col">
@@ -18,6 +19,19 @@ export function SessionCreateForm() {
         value={roomName}
         onChange={(e) => setRoomName(e.target.value)}
         onClear={() => setRoomName("")}
+        containerClassName="max-w-full"
+        className="max-w-full"
+      />
+
+      {/* 방 한줄 소개 */}
+      <Input
+        label="방 한줄 소개*"
+        placeholder="예) 1일 1목표를 달성하는 방이에요"
+        maxLength={50}
+        showCharacterCount
+        value={roomDescription}
+        onChange={(e) => setRoomDescription(e.target.value)}
+        onClear={() => setRoomDescription("")}
         containerClassName="max-w-full"
         className="max-w-full"
       />
