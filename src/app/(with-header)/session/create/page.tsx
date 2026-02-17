dc3460ea1df4a50c940accd590eebd53c40ca1bf
Rootjang92 <janggeunho7@gmail.com>
2026-02-17 09:10:13 +0900

    feat: 레이아웃, 제목

--- src/app/(with-header)/session/create/page.tsx
+++ src/app/(with-header)/session/create/page.tsx
@@ -1,7 +1,11 @@
 export default function SessionCreatePage() {
   return (
-    <div className="flex min-h-screen items-center justify-center">
-      <h1 className="text-2xl font-bold">Session Create Page</h1>
-    </div>
+    <main className="p-3xl w-full">
+      {/* 제목 섹션 */}
+      <header className="mb-2xl">
+        <h1 className="text-[24px] leading-[140%] font-bold text-gray-50">세션 만들기</h1>
+        <p className="mt-2xs text-base text-gray-500">함께 집중할 공간을 만들어보세요</p>
+      </header>
+    </main>
   );
 }
