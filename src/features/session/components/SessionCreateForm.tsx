abba9af261b2e219508401cd24a374627ff3b6ef
Rootjang92 <janggeunho7@gmail.com>
2026-02-17 09:24:44 +0900

    feat: 공지사항 부분 추가

--- src/features/session/components/SessionCreateForm.tsx
+++ src/features/session/components/SessionCreateForm.tsx
@@ -3,10 +3,12 @@
 import { useState } from "react";
 
 import { Input } from "@/components/Input/Input";
+import { Textarea } from "@/components/Textarea/Textarea";
 
 export function SessionCreateForm() {
   const [roomName, setRoomName] = useState("");
   const [roomDescription, setRoomDescription] = useState("");
+  const [notice, setNotice] = useState("");
 
   return (
     <form className="gap-xl flex w-full flex-col">
@@ -34,6 +36,18 @@ export function SessionCreateForm() {
         onClear={() => setRoomDescription("")}
         containerClassName="max-w-full"
         className="max-w-full"
+      />
+
+      {/* 공지사항 */}
+      <Textarea
+        label="공지사항"
+        placeholder="예) 세션의 규칙, 공지사항을 작성해주세요"
+        maxLength={100}
+        showCharacterCount
+        value={notice}
+        onChange={(e) => setNotice(e.target.value)}
+        containerClassName="max-w-full"
+        className="h-[260px] max-w-full"
       />
     </form>
   );
