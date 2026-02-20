a673b43d8820dec1efad86192a90a97dd991ac18
tnemnorivnelee <tnemnorivnelee@gmail.com>
2026-02-21 04:01:32 +0900

    fix: 구글 프로필 이미지 도메인 허용

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,11 @@ const nextConfig: NextConfig = {
         hostname: "example.com",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "/**",
+      },
     ],
   },
 };
