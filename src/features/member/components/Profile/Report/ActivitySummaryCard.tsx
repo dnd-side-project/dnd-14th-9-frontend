647a49a88db7528fd08f2ec4a5c0afcb602cf2ac
tnemnorivnelee <tnemnorivnelee@gmail.com>
2026-02-23 01:51:38 +0900

    feat: 나의 활동 요약 ProgressBar 추가

--- src/features/member/components/Profile/Report/ActivitySummaryCard.tsx
+++ src/features/member/components/Profile/Report/ActivitySummaryCard.tsx
@@ -1,7 +1,10 @@
+import { ProgressBar } from "@/components/ProgressBar/ProgressBar";
 import ReportCard from "@/components/ReportCard/ReportCard";
 import SectionTitle from "@/components/ReportCard/SectionTitle";
 
 export default function ActivitySummaryCard() {
+  const focusPercentage = 80;
+
   return (
     <ReportCard>
       <SectionTitle>나의 활동 요약</SectionTitle>
@@ -20,14 +23,15 @@ export default function ActivitySummaryCard() {
         <div className="bg-surface-strong px-lg py-md flex flex-col gap-[8px] rounded-sm">
           <div className="flex justify-between">
             <p className="text-text-secondary text-[16px] font-semibold">집중도</p>
-            <p className="text-text-primary text-[16px] font-semibold">80%</p>
+            <p className="text-text-primary text-[16px] font-semibold">{focusPercentage}%</p>
           </div>
-          <div>
-            {/** TODO(이경환): 게이지바 구현 */}
-            게이지바 영역
-          </div>
+          <ProgressBar
+            progress={focusPercentage}
+            className="bg-border-default h-[4px]"
+            indicatorClassName="bg-border-strong"
+          />
           <p className="text-text-muted font-regular text-[11px]">
-            전체 참여 시간 대비 80%를 집중했어요!
+            전체 참여 시간 대비 {focusPercentage}%를 집중했어요!
           </p>
         </div>
       </div>
