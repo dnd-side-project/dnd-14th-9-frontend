9480bdb5a7e83dbba2a4cf0c43f9875e06249d28
Rootjang92 <janggeunho7@gmail.com>
2026-02-24 08:26:14 +0900

    remove: 불필요한 파일 삭제

--- src/features/session/components/SessionResult/index.ts
+++
@@ -1,5 +0,0 @@
-export { SessionResultHeader } from "./SessionResultHeader";
-export { FocusTimeCard } from "./FocusTimeCard";
-export { ReceivedEmojiCard } from "./ReceivedEmojiCard";
-export { ActivitySummarySection } from "./ActivitySummarySection";
-export { GoalAchievementSection } from "./GoalAchievementSection";
