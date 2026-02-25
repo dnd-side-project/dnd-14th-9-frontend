fc6b30012e4015e1743dd32fa62ec598cbbdcdef
tnemn <63280112+tnemnorivnelee@users.noreply.github.com>
2026-02-25 10:35:33 +0900

    feat: 맞춤 추천 세션 디자인 업데이트 (#178)

--- src/features/session/components/RecommendedSection/EmptyRecommendedSessionPlaceholder.tsx
+++ src/features/session/components/RecommendedSection/EmptyRecommendedSessionPlaceholder.tsx
@@ -10,18 +10,18 @@ export function EmptyRecommendedSessionPlaceholder({
   categoryLabel,
 }: EmptyRecommendedSessionPlaceholderProps) {
   return (
-    <div className="bg-alpha-white-8 border-border-gray-default gap-xl p-4xl flex w-full flex-col items-center justify-center rounded-sm border border-solid">
-      <div className="font-pretendard text-text-primary text-center text-[24px] leading-[1.4] font-bold">
+    <div className="bg-surface-strong border-border-default gap-xl p-4xl flex w-full flex-col items-center justify-center rounded-sm border">
+      <div className="text-text-primary text-center text-[24px] leading-[1.4] font-bold">
         <p className="m-0">
           {nickname}님, <span className="text-surface-primary-default">{categoryLabel}</span>에도
           관심이 있으신가요?
         </p>
-        <p className="m-0">관련 세션을 만들어 보세요!</p>
+        <p className="m-0">그렇다면 세션을 직접 만들어보세요!</p>
       </div>
       <ButtonLink
         href="/session/create"
-        variant="solid"
-        colorScheme="primary"
+        variant="outlined"
+        colorScheme="secondary"
         size="medium"
         hardNavigate
       >
