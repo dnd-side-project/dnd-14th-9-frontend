b408739b65a262a355f795e4cc79e3e660246b73
tnemnorivnelee <tnemnorivnelee@gmail.com>
2026-05-13 01:38:40 +0900

    feat: SessionList에서 항상 list 타입 Pagination 사용하도록 변경

--- src/features/session/components/SessionList/SessionList.tsx
+++ src/features/session/components/SessionList/SessionList.tsx
@@ -154,22 +154,7 @@ export function SessionList() {
 
       {totalPage > 0 && (
         <div className="py-3xl flex justify-center">
-          {/* Mobile: fraction 타입 (< 1 / 3 >) */}
-          <Pagination
-            className="md:hidden"
-            type="fraction"
-            totalPage={totalPage}
-            currentPage={page}
-            onPageChange={setPage}
-          />
-          {/* Tablet+: list 타입 (← 이전 | 1 | 2 | 3 | ...10 | 다음 →) */}
-          <Pagination
-            className="hidden md:flex"
-            type="list"
-            totalPage={totalPage}
-            currentPage={page}
-            onPageChange={setPage}
-          />
+          <Pagination type="list" totalPage={totalPage} currentPage={page} onPageChange={setPage} />
         </div>
       )}
     </section>
