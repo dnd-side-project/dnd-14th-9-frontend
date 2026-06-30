3043d0eaa89275ecbcd9eee6f3facfed1ae4e268
tnemnorivnelee <tnemnorivnelee@gmail.com>
2026-06-30 16:55:09 +0900

    fix: mock auth redirect origin을 요청 기준으로 변경

--- src/mocks/handlers/auth.ts
+++ src/mocks/handlers/auth.ts
@@ -3,12 +3,12 @@ import { http, HttpResponse } from "msw";
 import { ok } from "./utils";
 
 export const authHandlers = [
-  http.get("*/api/auth/login", () => {
-    return HttpResponse.redirect(new URL("/", "http://localhost:3000"));
+  http.get("*/api/auth/login", ({ request }) => {
+    return HttpResponse.redirect(new URL("/", request.url));
   }),
 
-  http.get("*/api/auth/callback/:provider", () => {
-    return HttpResponse.redirect(new URL("/", "http://localhost:3000"));
+  http.get("*/api/auth/callback/:provider", ({ request }) => {
+    return HttpResponse.redirect(new URL("/", request.url));
   }),
 
   http.post("*/api/auth/logout", () => {
